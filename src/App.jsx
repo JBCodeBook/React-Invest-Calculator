a27d3cbe15b5d39bafc22cf1e45487254c84e2cc
jbuckl8 <jbuckl8@uwo.ca>
2024-07-28 18:41:12 -0400

    results class created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import Header from "./components/Header/Header"
+import Results from "./components/Results";
 import UserInput from "./components/UserInput"
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputId, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputId]: +newValue,
+      };
+    });
+  }
+
   return (
     <>
-    <Header />
-    <UserInput />
+      <Header />
+      <UserInput userInput= {userInput} onChange={handleChange} />
+      <Results userInput={userInput}/>
     </>
   )
 }
